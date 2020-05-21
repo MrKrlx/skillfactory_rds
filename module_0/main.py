b531ea6f71766b29feb1292f94543f0c9cc66e0f
Mr Krlx <a.kurinov@gmail.com>
2020-05-21 22:29:18 +0300

    Добавил третий варианта поиска загаданного числа

--- module_0/main.py
+++ module_0/main.py
@@ -13,14 +13,30 @@ def game_core_v2(number):
     '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
        Функция принимает загаданное число и возвращает число попыток'''
     count = 1
-    predict = np.random.randint(1,101)
+    predict = np.random.randint(1, 101)
     while number != predict:
-        count+=1
+        count += 1
         if number > predict:
             predict += 1
         elif number < predict:
             predict -= 1
     return(count) # выход из цикла, если угадали
+
+def game_core_v3(number):
+    count = 1
+    a = 1
+    b = 100
+    predict = (a + b) // 2
+    while number != predict:
+        count += 1
+        if number > predict:
+            a = predict + 1
+        elif number < predict:
+            b = predict - 1
+        predict = (a + b) // 2
+
+    return(count)
+
 
 def score_game(game_core):
     '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
@@ -39,3 +55,4 @@ def score_game(game_core):
 # запускаем
 score_game(game_core_v1)
 score_game(game_core_v2)
+score_game(game_core_v3)
